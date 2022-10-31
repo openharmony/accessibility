2d78cc8a83ace82e7c25da492d6efe99124fbd67
yichengzhao <yichengzhao1@huawei.com>
2022-10-31 15:46:10 +0800

    post dump to accessibility thread
    Signed-off-by: yichengzhao <yichengzhao1@huawei.com>
    Change-Id: I48277386d04151fd030e5e790f3abb577f1de5eb

--- services/aams/src/accessible_ability_manager_service.cpp
+++ services/aams/src/accessible_ability_manager_service.cpp
@@ -201,14 +201,23 @@ void AccessibleAbilityManagerService::OnRemoveSystemAbility(int32_t systemAbilit
 int AccessibleAbilityManagerService::Dump(int fd, const std::vector<std::u16string>& args)
 {
     HILOG_DEBUG("dump AccessibilityManagerServiceInfo");
-    if (!accessibilityDumper_) {
-        accessibilityDumper_ = new(std::nothrow) AccessibilityDumper();
-        if (!accessibilityDumper_) {
-            HILOG_ERROR("accessibilityDumper_ is nullptr");
-            return -1;
-        }
+    if (!handler_) {
+        HILOG_ERROR("Parameters check failed!");
+        return RET_ERR_NULLPTR;
     }
-    return accessibilityDumper_->Dump(fd, args);
+    std::promise<int> syncPromise;
+    std::future syncFuture = syncPromise.get_future();
+    handler_->PostTask(std::bind([this, &syncPromise, fd, args]() -> void {
+        if (!accessibilityDumper_) {
+            accessibilityDumper_ = new(std::nothrow) AccessibilityDumper();
+            if (!accessibilityDumper_) {
+                HILOG_ERROR("accessibilityDumper_ is nullptr");
+                syncPromise.set_value(-1);
+            }
+        }
+        syncPromise.set_value(accessibilityDumper_->Dump(fd, args));
+        }), "TASK_DUMP_INFO");
+    return syncFuture.get();
 }
 
 RetError AccessibleAbilityManagerService::SendEvent(const AccessibilityEventInfo &uiEvent)
