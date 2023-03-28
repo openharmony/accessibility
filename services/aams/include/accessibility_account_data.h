fdb6c60253e296f97fecda7d66b6617fe33c6ac4
luocheng <602796732@qq.com>
2023-03-28 21:49:02 +0800

    修改颜色反转问题 Signed-off-by: luocheng<luocheng52@huawei.com>

--- services/aams/include/accessibility_account_data.h
+++ services/aams/include/accessibility_account_data.h
@@ -237,6 +237,7 @@ public:
     void AddConfigCallback(const sptr<IAccessibleAbilityManagerConfigObserver>& callback);
     void RemoveConfigCallback(const wptr<IRemoteObject>& callback);
     const std::vector<sptr<IAccessibleAbilityManagerConfigObserver>> &GetConfigCallbacks() const;
+    void SetConfigCallbacks(std::vector<sptr<IAccessibleAbilityManagerConfigObserver>>& observer);
 
     void GetImportantEnabledAbilities(std::map<std::string, uint32_t> &importantEnabledAbilities) const;
     void UpdateImportantEnabledAbilities(std::map<std::string, uint32_t> &importantEnabledAbilities);
