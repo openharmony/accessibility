5b3da10790bdec8643ef483bb8dc5f9dc949da7d
luocheng <luocheng52@huawei.com>
2023-05-16 13:06:12 +0000

    update common/interface/src/accessibility_element_operator_callback_stub.cpp.
    Signed-off-by: luocheng <luocheng52@huawei.com>

--- common/interface/src/accessibility_element_operator_callback_stub.cpp
+++ common/interface/src/accessibility_element_operator_callback_stub.cpp
@@ -93,11 +93,7 @@ ErrCode AccessibilityElementOperatorCallbackStub::HandleSetSearchElementInfoByTe
     HILOG_DEBUG();
     std::vector<AccessibilityElementInfo> infos {};
     int32_t accessibilityInfosize = data.ReadInt32();
-    if ((&infos) == nullptr) {
-        HILOG_ERROR("Failed to read container due to val is nullptr");
-    } else {
-        ContainerSecurityVerify(data, accessibilityInfosize, infos.max_size());
-    }
+    ContainerSecurityVerify(data, accessibilityInfosize, infos.max_size());
     for (int32_t i = 0; i < accessibilityInfosize; i++) {
         sptr<AccessibilityElementInfoParcel> accessibilityInfo =
             data.ReadStrongParcelable<AccessibilityElementInfoParcel>();
