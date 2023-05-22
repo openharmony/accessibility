d94718873587417b45d468da2017abee33cc63d7
luocheng <luocheng52@huawei.com>
2023-05-22 08:35:18 +0000

    update
    Signed-off-by: luocheng <luocheng52@huawei.com>

--- interfaces/kits/napi/accessibility_extension_module_loader/src/napi_accessibility_extension.cpp
+++ interfaces/kits/napi/accessibility_extension_module_loader/src/napi_accessibility_extension.cpp
@@ -336,7 +336,7 @@ void NAccessibilityExtension::OnAccessibilityEvent(const AccessibilityEventInfo&
             napi_handle_scope scope = nullptr;
             NAPI_CALL(reinterpret_cast<napi_env>(data->engine_),
                 napi_open_handle_scope(reinterpret_cast<napi_env>(data->engine_), &scope));
-            auto closeScope = [(data->engine_)](napi_handle_scope scope) {
+            auto closeScope = [&(data->engine_)](napi_handle_scope scope) {
                 napi_close_handle_scope(reinterpret_cast<napi_env>(data->engine_), scope); };
             std::unique_ptr<napi_handle_scope__, decltype(closeScope)> scopes(scope, closeScope);
             napi_value napiEventInfo = nullptr;
@@ -406,7 +406,7 @@ bool NAccessibilityExtension::OnKeyPressEvent(const std::shared_ptr<MMI::KeyEven
             napi_handle_scope scope = nullptr;
             NAPI_CALL(reinterpret_cast<napi_env>(data->engine_),
                 napi_open_handle_scope(reinterpret_cast<napi_env>(data->engine_), &scope));
-            auto closeScope = [(data->engine_)](napi_handle_scope scope) {
+            auto closeScope = [&(data->engine_)](napi_handle_scope scope) {
                 napi_close_handle_scope(reinterpret_cast<napi_env>(data->engine_), scope); };
             std::unique_ptr<napi_handle_scope__, decltype(closeScope)> scopes(scope, closeScope);
             napi_value napiEventInfo = nullptr;
