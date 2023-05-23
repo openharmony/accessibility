2183d748dd6a5058e93ddfd0b992e2cfdb5ebc98
luocheng <luocheng52@huawei.com>
2023-05-23 06:24:09 +0000

    update services/aams/include/accessibility_touch_guider.h.
    Signed-off-by: luocheng <luocheng52@huawei.com>

--- services/aams/include/accessibility_touch_guider.h
+++ services/aams/include/accessibility_touch_guider.h
@@ -420,7 +420,9 @@ private:
     std::shared_ptr<TGEventHandler> handler_ = nullptr;
     std::shared_ptr<AppExecFwk::EventRunner> runner_ = nullptr;
 };
-bool HWTEST_FACCESSIBILITY(uint32_t x);
+bool HWTEST_FEVENTTYPE(uint32_t x);
+bool HWTEST_FTOUCHEVENT0(uint32_t x);
+bool HWTEST_FTOUCHEVENT1(uint32_t x);
 } // namespace Accessibility
 } // namespace OHOS
 #endif // ACCESSIBILITY_TOUCH_GUIDER_H
