c1e277e550b2038827ffcd49a1a2ab2408013c46
luocheng <luocheng52@huawei.com>
2023-06-21 06:30:43 +0000

    update common/interface/include/accessibility_icp_interface_code.h.
    Signed-off-by: luocheng <luocheng52@huawei.com>

--- common/interface/include/accessibility_icp_interface_code.h
+++ common/interface/include/accessibility_icp_interface_code.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,7 +18,7 @@
 /* SAID:3503 */
 namespace OHOS {
 namespace Accessibility {
-    enum class AccessibilityInterfaceCode{
+    enum class AccessibilityInterfaceCode {
 
         SET_RESULT_BY_ACCESSIBILITY_ID = 100,
         SET_RESULT_BY_TEXT,
