823a38f6e8dc50c6fb89f2325464aeb583c6507d
luocheng <luocheng52@huawei.com>
2023-06-27 01:39:04 +0000

    update common/interface/src/accessible_ability_channel_stub.cpp.
    Signed-off-by: luocheng <luocheng52@huawei.com>

--- common/interface/src/accessible_ability_channel_stub.cpp
+++ common/interface/src/accessible_ability_channel_stub.cpp
@@ -17,7 +17,7 @@
 #include "accessibility_element_info_parcel.h"
 #include "accessibility_gesture_inject_path_parcel.h"
 #include "accessibility_window_info_parcel.h"
-#include "accessibility_icp_interface_code.h"
+#include "accessibility_ipc_interface_code.h"
 #include "hilog_wrapper.h"
 #include "parcel_util.h"
 
