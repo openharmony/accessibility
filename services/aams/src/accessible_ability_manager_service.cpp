8cb1b5ff3497dbb657fb2e7114aa333438966c37
luocheng <luocheng52@huawei.com>
2023-06-28 03:42:25 +0000

    update services/aams/src/accessible_ability_manager_service.cpp.
    Signed-off-by: luocheng <luocheng52@huawei.com>

--- services/aams/src/accessible_ability_manager_service.cpp
+++ services/aams/src/accessible_ability_manager_service.cpp
@@ -269,7 +269,7 @@ uint32_t AccessibleAbilityManagerService::RegisterStateObserver(
     const sptr<IAccessibleAbilityManagerStateObserver> &callback)
 {
     HILOG_DEBUG();
-    std::lock_guard<std::mutex> lock(mutex_);
+    
     if (!callback || !handler_) {
         HILOG_ERROR("Parameters check failed!");
         return 0;
@@ -293,10 +293,13 @@ uint32_t AccessibleAbilityManagerService::RegisterStateObserver(
             return;
         }
         callback->AsObject()->AddDeathRecipient(stateCallbackDeathRecipient_);
-        auto iter = std::find(stateCallbacks_.begin(), stateCallbacks_.end(), callback);
-        if (iter == stateCallbacks_.end()) {
-            stateCallbacks_.push_back(callback);
-            HILOG_INFO("RegisterStateObserver successfully");
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            auto iter = std::find(stateCallbacks_.begin(), stateCallbacks_.end(), callback);
+            if (iter == stateCallbacks_.end()) {
+                stateCallbacks_.push_back(callback);
+                HILOG_INFO("RegisterStateObserver successfully");
+            }
         }
 
         sptr<AccessibilityAccountData> accountData = GetCurrentAccountData();
@@ -1221,7 +1224,6 @@ void AccessibleAbilityManagerService::UpdateAccessibilityManagerService()
 void AccessibleAbilityManagerService::UpdateAccessibilityState()
 {
     HILOG_DEBUG("start.");
-    std::lock_guard<std::mutex> lock(mutex_);
     sptr<AccessibilityAccountData> accountData = GetCurrentAccountData();
     if (!accountData) {
         HILOG_ERROR("Account data is null");
@@ -1231,6 +1233,7 @@ void AccessibleAbilityManagerService::UpdateAccessibilityState()
     if (!(state & STATE_ACCESSIBILITY_ENABLED)) {
         Singleton<AccessibilityWindowManager>::GetInstance().ClearAccessibilityFocused();
     }
+    std::lock_guard<std::mutex> lock(mutex_);
     for (auto &callback : stateCallbacks_) {
         if (callback) {
             callback->OnStateChanged(state);
@@ -2156,7 +2159,6 @@ void AccessibleAbilityManagerService::RemoveCallback(CallBackID callback,
     const sptr<DeathRecipient> &recipient, const wptr<IRemoteObject> &remote)
 {
     HILOG_INFO("remove callback[%{public}d]", callback);
-    std::lock_guard<std::mutex> lock(mutex_);
     if (!handler_) {
         HILOG_ERROR("handler is nullptr");
         return;
@@ -2176,6 +2178,7 @@ void AccessibleAbilityManagerService::RemoveCallback(CallBackID callback,
         switch (callback) {
             case STATE_CALLBACK:
                 {
+                    std::lock_guard<std::mutex> lock(mutex_);
                     auto iter = std::find_if(stateCallbacks_.begin(), stateCallbacks_.end(),
                         [remote](const sptr<IAccessibleAbilityManagerStateObserver> &stateCallback) {
                             return stateCallback->AsObject() == remote;
