70088e819fe244567a8bbf85bf9ec6b58f459b9e
jinqi6 <jinqi6@huawei.com>
2023-07-05 05:58:50 +0000

    update services/aams/src/accessible_ability_channel.cpp. 无障碍修改异步函数 Signed-off-by: jinqi6 <jinqi6@huawei.com>
    Signed-off-by: jinqi6 <jinqi6@huawei.com>

--- services/aams/src/accessible_ability_channel.cpp
+++ services/aams/src/accessible_ability_channel.cpp
@@ -247,9 +247,9 @@ RetError AccessibleAbilityChannel::GetWindow(const int32_t windowId, Accessibili
         HILOG_ERROR("Failed to wait GetWindow result");
         return RET_ERR_TIME_OUT;
     }
-    RetError ret = syncFuture.get();
+
     windowInfo = *tmpWindowInfo;
-    return ret;
+    return syncFuture.get();;
 }
 
 RetError AccessibleAbilityChannel::GetWindows(std::vector<AccessibilityWindowInfo> &windows)
@@ -308,9 +308,9 @@ RetError AccessibleAbilityChannel::GetWindows(uint64_t displayId, std::vector<Ac
         HILOG_ERROR("Failed to wait GetWindows result");
         return RET_ERR_TIME_OUT;
     }
-    RetError ret = syncFuture.get();
+
     windows = *tmpWindows;
-    return ret;
+    return syncFuture.get();
 }
 
 void AccessibleAbilityChannel::SetOnKeyPressEventResult(const bool handled, const int32_t sequence)
