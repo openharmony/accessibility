6e444b7eaab92ac95291351f7344231c140e6e2c
wangxinqiang007 <wangxinqiang2@huawei.com>
2023-11-01 22:06:11 +0800

    增加校验查询节点的结果
    Signed-off-by: wangxinqiang007 <wangxinqiang2@huawei.com>

--- frameworks/aafwk/include/accessible_ability_client_impl.h
+++ frameworks/aafwk/include/accessible_ability_client_impl.h
@@ -374,6 +374,8 @@ private:
         const uint32_t mode, AccessibilityElementInfo &info);
     bool InitAccessibilityServiceProxy();
     static void OnParameterChanged(const char *key, const char *value, void *context);
+    RetError CheckElementInfo(int32_t windowId, int32_t elementId, int mode,
+        std::vector<AccessibilityElementInfo> &elementInfos);
 
     sptr<IRemoteObject::DeathRecipient> deathRecipient_ = nullptr;
     sptr<IRemoteObject::DeathRecipient> accessibilityServiceDeathRecipient_ = nullptr;
