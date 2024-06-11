58fb1612b551f335146ad556fb50831ebdb370d1
zwx1339610 <zhangyu841@h-partners.com>
2024-06-11 17:39:11 +0800

    获取表格行和列信息
    Signed-off-by: zwx1339610 <zhangyu841@h-partners.com>

--- interfaces/kits/napi/accessibility_extension_module_loader/src/napi_accessibility_element.cpp
+++ interfaces/kits/napi/accessibility_extension_module_loader/src/napi_accessibility_element.cpp
@@ -34,7 +34,7 @@ namespace {
         "selected", "clickable", "longClickable", "isEnable", "isPassword", "scrollable",
         "editable", "pluralLineSupported", "parent", "children", "isFocused", "accessibilityFocused",
         "error", "isHint", "pageId", "valueMax", "valueMin", "valueNow", "windowId", "accessibilityText",
-        "textType", "offset", "allAttribute"};
+        "textType", "offset", "currentItem", "allAttribute"};
     const std::vector<std::string> WINDOW_INFO_ATTRIBUTE_NAMES = {"isActive", "screenRect", "layer", "type",
         "rootElement", "isFocused", "windowId"};
 
@@ -85,6 +85,7 @@ namespace {
         {"accessibilityText", &NAccessibilityElement::GetElementInfoAccessibilityText},
         {"textType", &NAccessibilityElement::GetElementInfoTextType},
         {"offset", &NAccessibilityElement::GetElementInfoOffset},
+        {"currentItem", &NAccessibilityElement::GetElementInfoGridItem},
         {"allAttribute", &NAccessibilityElement::GetElementInfoAllAttribute},
     };
     std::map<std::string, AttributeNamesFunc> windowInfoCompleteMap = {
@@ -578,6 +579,16 @@ void NAccessibilityElement::GetElementInfoRect(NAccessibilityElementData *callba
     ConvertRectToJS(callbackInfo->env_, value, screenRect);
 }
 
+void NAccessibilityElement::GetElementInfoGridItem(NAccessibilityElementData *callbackInfo, napi_value &value)
+{
+    if (!CheckElementInfoParameter(callbackInfo, value)) {
+        return;
+    }
+    OHOS::Accessibility::GridItemInfo gridItem = callbackInfo->accessibilityElement_.elementInfo_->GetGridItem();
+    NAPI_CALL_RETURN_VOID(callbackInfo->env_, napi_create_object(callbackInfo->env_, &value));
+    ConvertGridItemToJS(callbackInfo->env_, value, gridItem);
+}
+
 void NAccessibilityElement::GetElementInfoCheckable(NAccessibilityElementData *callbackInfo, napi_value &value)
 {
     if (!CheckElementInfoParameter(callbackInfo, value)) {
@@ -1041,6 +1052,10 @@ void NAccessibilityElement::GetElementInfoAllAttribute3(NAccessibilityElementDat
     napi_value itemCount = nullptr;
     GetElementInfoItemCount(callbackInfo, itemCount);
     NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, value, "itemCount", itemCount));
+
+    napi_value grid = nullptr;
+    GetElementInfoGridItem(callbackInfo, grid);
+    NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, value, "grid", grid));
 
     napi_value currentIndex = nullptr;
     GetElementInfoCurrentIndex(callbackInfo, currentIndex);
