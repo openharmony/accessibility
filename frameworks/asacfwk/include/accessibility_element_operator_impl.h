075868895dbcf64b8fd5ff2e612e8dcb55999772
taojuncun <taojuncun@huawei.com>
2024-08-13 16:36:13 +0800

    addrequest稳定性问题修复
    Signed-off-by: taojuncun <taojuncun@huawei.com>

--- frameworks/asacfwk/include/accessibility_element_operator_impl.h
+++ frameworks/asacfwk/include/accessibility_element_operator_impl.h
@@ -214,7 +214,6 @@ public:
      * @param requestId The request id from AA, it is used to match with request and response.
      */
     void SetCursorPositionResult(const int32_t cursorPosition, const int32_t requestId);
-    static int32_t GetWindIdByRequestId(const int32_t requestId);
 
     static sptr<IAccessibilityElementOperatorCallback> GetCallbackByRequestId(const int32_t requestId);
     static void EraseCallback(const int32_t requestId);
@@ -222,12 +221,11 @@ public:
 private:
     int32_t AddRequest(int32_t requestId, const sptr<IAccessibilityElementOperatorCallback> &callback);
 
-    std::mutex mutex_;
+    static std::mutex requestsMutex_;
     int32_t windowId_ = 0;
     AccessibilityElementOperatorCallback &operatorCallback_;
     std::shared_ptr<AccessibilityElementOperator> operator_ = nullptr;
     static std::unordered_map<int32_t, sptr<IAccessibilityElementOperatorCallback>> requests_;
-    static std::unordered_map<int32_t, int32_t> requestWindId_; //key requestid
     DISALLOW_COPY_AND_MOVE(AccessibilityElementOperatorImpl);
 };
 
