0210706992410ef40fdce07444f2c617aab505cd
fanzexuan <fanzexuan@huawei.com>
2024-09-12 17:27:05 +0800

    无障碍限制后台唤醒
    Signed-off-by: fanzexuan <fanzexuan@huawei.com>

--- common/interface/src/accessible_ability_manager_caption_observer_proxy.cpp
+++ common/interface/src/accessible_ability_manager_caption_observer_proxy.cpp
@@ -45,7 +45,7 @@ void AccessibleAbilityManagerCaptionObserverProxy::OnPropertyChanged(
     int32_t error = NO_ERROR;
     MessageParcel data;
     MessageParcel reply;
-    MessageOption option(MessageOption::TF_ASYNC);
+    MessageOption option(MessageOption::TF_ASYNC | MessageOption::TF_ASYNC_WAKEUP_LATER);
     CaptionPropertyParcel captionParcel(property);
 
     if (!WriteInterfaceToken(data)) {
