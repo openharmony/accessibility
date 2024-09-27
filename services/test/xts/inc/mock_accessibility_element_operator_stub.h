519cbd04e930b449545b45303a4a134140d13d64
taojuncun <taojuncun@huawei.com>
2024-09-27 15:37:59 +0800

    无障碍内存xts用例整理
    Signed-off-by: taojuncun <taojuncun@huawei.com>

---
+++ services/test/xts/inc/mock_accessibility_element_operator_stub.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOCK_ACCESSIBILITY_ELEMENT_OPERATOR_STUB_H
+#define MOCK_ACCESSIBILITY_ELEMENT_OPERATOR_STUB_H
+
+#include <gmock/gmock.h>
+#include "accessibility_element_operator_stub.h"
+
+namespace OHOS {
+namespace A11y {
+class MockA11yElementOperatorStub : public A11yElementOperatorStub {
+public:
+    MockA11yElementOperatorStub() = default;
+    virtual ~MockA11yElementOperatorStub() = default;
+
+    MOCK_METHOD4(OnRemoteRequest, int(uint32_t code, MessageParcel& data, MessageParcel& reply, MessageOption& option));
+    MOCK_METHOD5(SearchElementInfoByA11yId,
+        void(const int64_t elementId, const int32_t requestId,
+            const sptr<IA11yElementOperatorCallback>& callback, const int32_t mode, bool isFilter));
+    MOCK_METHOD4(
+        SearchElementInfosByText, void(const int64_t elementId, const std::string& text, const int32_t requestId,
+                                      const sptr<IA11yElementOperatorCallback>& callback));
+    MOCK_METHOD4(FindFocusedElementInfo, void(const int64_t elementId, const int32_t focusType, const int32_t requestId,
+                                             const sptr<IA11yElementOperatorCallback>& callback));
+    MOCK_METHOD4(FocusMoveSearch, void(const int64_t elementId, const int32_t direction, const int32_t requestId,
+                                      const sptr<IA11yElementOperatorCallback>& callback));
+    MOCK_METHOD5(ExecuteAction,
+        void(const int64_t elementId, const int32_t action, const std::map<std::string, std::string> &actionArguments,
+            const int32_t requestId, const sptr<IA11yElementOperatorCallback>& callback));
+    MOCK_METHOD3(GetCursorPosition,
+        void(const int64_t elementId, const int32_t requestId,
+        const sptr<IA11yElementOperatorCallback>& callback));
+    MOCK_METHOD0(ClearFocus, void());
+    MOCK_METHOD0(OutsideTouch, void());
+    MOCK_METHOD1(SetWindowId, void(int32_t windowId));
+    MOCK_METHOD0(GetWindowId, int32_t());
+    MOCK_METHOD3(SetChildTreeIdAndWinId, void(const int64_t elementId, const int32_t treeId,
+        const int32_t childWindowId));
+    MOCK_METHOD1(SetBelongTreeId, void(const int32_t treeId));
+    MOCK_METHOD1(SetParentWindowId, void(const int32_t parentWindowId));
+};
+} // namespace A11y
+} // namespace OHOS
+#endif // MOCK_ACCESSIBILITY_ELEMENT_OPERATOR_STUB_H
