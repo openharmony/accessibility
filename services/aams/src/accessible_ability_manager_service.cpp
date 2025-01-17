7516f44f7b1520230c6e8a03536f9bf83a7b9ac2
taojuncun <taojuncun@huawei.com>
2025-01-17 15:03:00 +0800

    修复注销场景，windowId下的子树可能未清理死亡回调的问题
    Signed-off-by: taojuncun <taojuncun@huawei.com>

--- services/aams/src/accessible_ability_manager_service.cpp
+++ services/aams/src/accessible_ability_manager_service.cpp
@@ -1074,7 +1074,6 @@ RetError AccessibleAbilityManagerService::DeregisterElementOperator(int32_t wind
             HILOG_WARN("The operation of windowId[%{public}d] has not been registered.", windowId);
             return;
         }
-        accountData->RemoveAccessibilityWindowConnection(windowId);
         StopCallbackWait(windowId);
 
         if (!connection->GetProxy()) {
@@ -1093,6 +1092,13 @@ RetError AccessibleAbilityManagerService::DeregisterElementOperator(int32_t wind
             } else {
                 HILOG_INFO("cannot find remote object. windowId[%{public}d]", windowId);
             }
+        }
+
+        connection->SetProxy(nullptr);
+        RemoveTreeDeathRecipient(windowId, 0, connection);
+        // remove connection when all proxy and children tree proxy deregistered
+        if (connection->GetProxy() == nullptr && connection->GetCardProxySize() == 0) {
+            accountData->RemoveAccessibilityWindowConnection(windowId);
         }
         }, "TASK_DEREGISTER_ELEMENT_OPERATOR");
     return RET_OK;
@@ -1120,14 +1126,10 @@ RetError AccessibleAbilityManagerService::DeregisterElementOperator(int32_t wind
         }
         StopCallbackWait(windowId, treeId);
 
-        if (!connection->GetCardProxy(treeId)) {
-            HILOG_ERROR("proxy is null");
-            return;
-        }
-
-        auto object = connection->GetCardProxy(treeId)->AsObject();
-        if (object) {
-            RemoveTreeDeathRecipient(windowId, treeId, connection);
+        RemoveTreeDeathRecipient(windowId, treeId, connection);
+        // remove connection when all proxy and children tree proxy deregistered
+        if (connection->GetProxy() == nullptr && connection->GetCardProxySize() == 0) {
+            accountData->RemoveAccessibilityWindowConnection(windowId);
         }
         }, "TASK_DEREGISTER_ELEMENT_OPERATOR");
     return RET_OK;
