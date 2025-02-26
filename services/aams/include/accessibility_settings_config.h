0f2401c35487ef0b3bec97118702ed343129003e
lsl <lanshulei@huawei.com>
2025-02-26 11:45:23 +0000

    update services/aams/include/accessibility_settings_config.h.
    Signed-off-by: lsl <lanshulei@huawei.com>

--- services/aams/include/accessibility_settings_config.h
+++ services/aams/include/accessibility_settings_config.h
@@ -148,7 +148,6 @@ private:
     std::shared_ptr<AccessibilityDatashareHelper> datashare_ = nullptr;
     std::shared_ptr<AccessibilityDatashareHelper> systemDatashare_ = nullptr;
     ffrt::mutex interfaceMutex_;
-    bool isInitialized_ = false;
 };
 } // namespace Accessibility
 } // namespace OHOS
