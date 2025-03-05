25103612b610c0c103afc8129626abe3c8e60026
lsl <lanshulei@huawei.com>
2025-03-05 01:04:33 +0000

    18
    Signed-off-by: lsl <lanshulei@huawei.com>

--- services/aams/src/accessibility_account_data.cpp
+++ services/aams/src/accessibility_account_data.cpp
@@ -1038,7 +1038,11 @@ void AccessibilityAccountData::AddUITestClient(const sptr<IRemoteObject> &obj,
     HILOG_DEBUG();
     // Add installed ability
     std::shared_ptr<AccessibilityAbilityInfo> abilityInfo = std::make_shared<AccessibilityAbilityInfo>();
+    if (abilityInfo == nullptr) {
+        return;
+    }
     abilityInfo->SetPackageName(bundleName);
+    abilityInfo->SetName(abilityName);
     uint32_t capabilities = CAPABILITY_RETRIEVE | CAPABILITY_GESTURE;
     abilityInfo->SetCapabilityValues(capabilities);
     abilityInfo->SetAccessibilityAbilityType(ACCESSIBILITY_ABILITY_TYPE_ALL);
