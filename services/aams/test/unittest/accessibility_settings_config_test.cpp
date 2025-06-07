21069f86f66bfdfaf649fb6e5ab2fd8069d2da97
lsl <lanshulei@huawei.com>
2025-06-07 11:46:53 +0000

    1
    Signed-off-by: lsl <lanshulei@huawei.com>

--- services/aams/test/unittest/accessibility_settings_config_test.cpp
+++ services/aams/test/unittest/accessibility_settings_config_test.cpp
@@ -489,7 +489,7 @@ HWTEST_F(AccessibilitySettingsConfigTest, AccessibilitySettingsConfig_Unittest_S
     GTEST_LOG_(INFO) << "AccessibilitySettingsConfig_Unittest_SetMouseAutoClick_002 start";
     settingConfig_->Init();
     settingConfig_->SetMouseAutoClick(MOUSE_AUTO_CLICK_VALUE);
-    EXPECT_EQ(-1, settingConfig_->GetMouseAutoClick());
+    EXPECT_EQ(MOUSE_AUTO_CLICK_VALUE, settingConfig_->GetMouseAutoClick());
     GTEST_LOG_(INFO) << "AccessibilitySettingsConfig_Unittest_SetMouseAutoClick_002 end";
 }
 
@@ -521,7 +521,7 @@ HWTEST_F(AccessibilitySettingsConfigTest, AccessibilitySettingsConfig_Unittest_S
     std::string name = "TEST";
     settingConfig_->Init();
     settingConfig_->SetShortkeyTarget(name);
-    EXPECT_STREQ("", settingConfig_->GetShortkeyTarget().c_str());
+    EXPECT_STREQ("TEST", settingConfig_->GetShortkeyTarget().c_str());
     GTEST_LOG_(INFO) << "AccessibilitySettingsConfig_Unittest_SetShortkeyTarget_002 end";
 }
 
