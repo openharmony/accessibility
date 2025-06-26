03cbde927d5262fb66964e1051c4814ac2758159
xueyuanzhao <zhaoxueyuan@huawei.com>
2025-06-26 17:30:05 +0800

    Signed-off-by:xueyuanzhao<zhaoxueyuan@huawei.com>
    Signed-off-by: xueyuanzhao <zhaoxueyuan@huawei.com>
    Change-Id: I85f28855be2a68f565fe63a1abd7f0829414bdeb

--- services/test/mock/multimodalinput/src/mock_input_manager.cpp
+++ services/test/mock/multimodalinput/src/mock_input_manager.cpp
@@ -87,7 +87,8 @@ void InputManager::SimulateInputEvent(std::shared_ptr<KeyEvent> keyEvent)
     mockKeyCode = keyEvent->GetKeyCode();
 }
 
-void InputManager::SimulateInputEvent(std::shared_ptr<PointerEvent> pointerEvent, bool isAutoToVirtualScreen)
+void InputManager::SimulateInputEvent(std::shared_ptr<PointerEvent> pointerEvent, bool isAutoToVirtualScreen,
+    int32_t useCoordinate)
 {
     HILOG_DEBUG();
     std::lock_guard<ffrt::mutex> lock(g_mtx);
