21958dd176c093e7cfc63b81345ab3ab06ce24a3
lidalonggit <lixun26@huawei.com>
2025-08-27 14:48:06 +0800

    告警问题
    Signed-off-by: lidalonggit <lixun26@huawei.com>

--- common/interface/src/parcel/accessibility_window_info_parcel.cpp
+++ common/interface/src/parcel/accessibility_window_info_parcel.cpp
@@ -52,7 +52,7 @@ bool AccessibilityWindowInfoParcel::ReadFromParcel(Parcel &parcel)
     int32_t touchHotAreasSize = 0;
     static const int32_t touchHotAreasMaxSize = 1024;
     READ_PARCEL_AND_RETURN_FALSE_IF_FAIL(Int32, parcel, touchHotAreasSize);
-    if (touchHotAreasSize > touchHotAreasMaxSize) {
+    if (touchHotAreasSize > touchHotAreasMaxSize || touchHotAreasSize < 0) {
         return false;
     }
     std::vector<Rect> touchHotAreas {};
