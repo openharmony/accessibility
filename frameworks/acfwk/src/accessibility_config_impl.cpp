a793ac8efd96dfdc49c4cf5c854a18feca6ebdce
liuzhiwei67 <liuzhiwei67@h-partners.com>
2025-09-12 16:19:22 +0800

    Get captions style
    Signed-off-by: liuzhiwei67 <liuzhiwei67@h-partners.com>

--- frameworks/acfwk/src/accessibility_config_impl.cpp
+++ frameworks/acfwk/src/accessibility_config_impl.cpp
@@ -398,6 +398,7 @@ Accessibility::RetError AccessibilityConfig::Impl::GetCaptionsProperty(CaptionPr
     CaptionPropertyParcel captionParcel(caption);
     Accessibility::RetError ret = static_cast<Accessibility::RetError>(
         GetServiceProxy()->GetCaptionProperty(captionParcel));
+    caption = captionParcel;
     HILOG_INFO();
     return ret;
 }
