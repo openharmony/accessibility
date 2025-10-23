c536ca04b7ee8a5e8d4064f193af9c1c29723377
qianchuang <923384386@qq.com>
2025-10-23 19:12:13 +0800

    fuzz优化
    Signed-off-by: qianchuang <923384386@qq.com>

---
+++ common/interface/test/fuzztest/accessibleabilitychannelstub/accessibleabilitychannelstubgetwindow_fuzzer/accessibleabilitychannelstubgetwindow_fuzzer.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "accessibleabilitychannelstubgetwindow_fuzzer.h"
+#include "accessible_ability_channel_stub.h"
+#include "accessibility_ipc_interface_code.h"
+#include "parcel/accessibility_gesture_inject_path_parcel.h"
+#include "securec.h"
+#include "abilit_channel_impl_fuzz_test.h"
+
+namespace OHOS {
+namespace Accessibility {
+
+bool FuzzHandleGetWindow(const uint8_t *data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return false;
+    }
+
+    size_t position = 0;
+    int32_t windowId = 0;
+    MessageParcel mdata;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_SYNC);
+    GetObject<int32_t>(windowId, &data[position], size - position);
+    std::shared_ptr<AbilityChannelImplFuzzTest> chanImp = std::make_shared<AbilityChannelImplFuzzTest>();
+    if (chanImp == nullptr) {
+        return false;
+    }
+    mdata.WriteInterfaceToken(AccessibleAbilityChannelStub::GetDescriptor());
+    mdata.WriteInt32(windowId);
+    chanImp->OnRemoteRequest(static_cast<uint32_t>(AccessibilityInterfaceCode::GET_WINDOW),
+        mdata, reply, option);
+    mdata.RewindRead(0);
+    chanImp->OnRemoteRequest(static_cast<uint32_t>(AccessibilityInterfaceCode::GET_WINDOWS),
+        mdata, reply, option);
+    return true;
+}
+
+bool FuzzHandleGetWindows()
+{
+    MessageParcel mdata;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_SYNC);
+
+    std::shared_ptr<AbilityChannelImplFuzzTest> chanImp = std::make_shared<AbilityChannelImplFuzzTest>();
+    if (chanImp == nullptr) {
+        return false;
+    }
+    mdata.WriteInterfaceToken(AccessibleAbilityChannelStub::GetDescriptor());
+    chanImp->OnRemoteRequest(static_cast<uint32_t>(AccessibilityInterfaceCode::GET_WINDOWS),
+        mdata, reply, option);
+    return true;
+}
+
+bool FuzzHandleGetWindowsByDisplayId(const uint8_t *data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return false;
+    }
+
+    size_t position = 0;
+    uint64_t displayId = 0;
+    MessageParcel mdata;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_SYNC);
+    GetObject<uint64_t>(displayId, &data[position], size - position);
+    std::shared_ptr<AbilityChannelImplFuzzTest> chanImp = std::make_shared<AbilityChannelImplFuzzTest>();
+    if (chanImp == nullptr) {
+        return false;
+    }
+    mdata.WriteInterfaceToken(AccessibleAbilityChannelStub::GetDescriptor());
+    mdata.WriteUint64(displayId);
+    chanImp->OnRemoteRequest(static_cast<uint32_t>(AccessibilityInterfaceCode::GET_WINDOWS_BY_DISPLAY_ID),
+        mdata, reply, option);
+    return true;
+}
+
+bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+{
+    FuzzHandleGetWindow(data, size);
+    FuzzHandleGetWindowsByDisplayId(data, size);
+    return true;
+}
+} // namespace Accessibility
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Accessibility::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
---
+++ common/interface/test/fuzztest/accessibleabilitychannelstub/accessibleabilitychannelstubsetbundlename_fuzzer/accessibleabilitychannelstubsetbundlename_fuzzer.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "accessibleabilitychannelstubsetbundlename_fuzzer.h"
+#include "accessible_ability_channel_stub.h"
+#include "accessibility_ipc_interface_code.h"
+#include "parcel/accessibility_gesture_inject_path_parcel.h"
+#include "securec.h"
+#include "abilit_channel_impl_fuzz_test.h"
+
+namespace OHOS {
+namespace Accessibility {
+
+bool FuzzHandleSetTargetBundleName(const uint8_t *data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return false;
+    }
+
+    size_t positions = 0;
+    MessageParcel mdata;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_SYNC);
+    char name[LEN + 1];
+    name[LEN] = END_CHAR;
+    for (size_t i = 0; i < LEN; i++) {
+        positions += GetObject<char>(name[i], data + positions, size - positions);
+    }
+    std::string bundleName(name);
+    std::shared_ptr<AbilityChannelImplFuzzTest> chanImp = std::make_shared<AbilityChannelImplFuzzTest>();
+    if (chanImp == nullptr) {
+        return false;
+    }
+    mdata.WriteInterfaceToken(AccessibleAbilityChannelStub::GetDescriptor());
+    mdata.WriteInt32(LEN);
+    for (size_t i = 0; i < LEN; i++) {
+        mdata.WriteString(bundleName);
+    }
+    chanImp->OnRemoteRequest(static_cast<uint32_t>(AccessibilityInterfaceCode::SET_TARGET_BUNDLE_NAME),
+        mdata, reply, option);
+    return true;
+}
+
+bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+{
+    FuzzHandleSetTargetBundleName(data, size);
+    return true;
+}
+} // namespace Accessibility
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Accessibility::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
