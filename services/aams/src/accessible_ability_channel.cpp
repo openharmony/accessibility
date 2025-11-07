f76f3378b607a9a8d8eab8ec8502394c1d7f3d1a
nsdaf <lanshulei@huawei.com>
2025-11-07 15:47:24 +0800

    update: 更新文件 accessible_ability_channel.cpp 1
    Signed-off-by: nsdaf <lanshulei@huawei.com>

--- services/aams/src/accessible_ability_channel.cpp
+++ services/aams/src/accessible_ability_channel.cpp
@@ -277,7 +277,7 @@ RetError AccessibleAbilityChannel::FindFocusedElementInfo(const int32_t accessib
         }
         auto& awm = Singleton<AccessibilityWindowManager>::GetInstance();
         int64_t realElementId = ROOT_NODE_ID;
-        if (treeId < 0) {
+        if (treeId <= 0) {
             realElementId = awm.GetSceneBoardElementId(accessibilityWindowId, elementId);
         }
         Singleton<AccessibleAbilityManagerService>::GetInstance().AddRequestId(accessibilityWindowId, treeId,
