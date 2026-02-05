a97eacd280360936f3638693c2948462575531cb
Chanlung <yejialong2@h-partners.com>
2026-02-05 10:18:40 +0800

    reconstruct code
    Signed-off-by: Chanlung <yejialong2@h-partners.com>

--- interfaces/kits/napi/src/napi_accessibility_system_ability_client.cpp
+++ interfaces/kits/napi/src/napi_accessibility_system_ability_client.cpp
@@ -75,17 +75,15 @@ napi_value NAccessibilityClient::IsScreenReaderOpenSync(napi_env env, napi_callb
     size_t argc = ARGS_SIZE_ONE;
     napi_value argv = nullptr;
     bool status = false;
-    do {
-        napi_get_cb_info(env, info, &argc, &argv, nullptr, nullptr);
-        if (argc != ARGS_SIZE_ZERO) {
-            break;
-        }
-        auto asaClient = AccessibilitySystemAbilityClient::GetInstance();
-        if (asaClient == nullptr) {
-            break;
-        }
-        asaClient->IsScreenReaderEnabled(status);
-    } while (0);
+    napi_get_cb_info(env, info, &argc, &argv, nullptr, nullptr);
+    if (argc != ARGS_SIZE_ZERO) {
+        break;
+    }
+    auto asaClient = AccessibilitySystemAbilityClient::GetInstance();
+    if (asaClient == nullptr) {
+        break;
+    }
+    asaClient->IsScreenReaderEnabled(status);
     napi_value result = nullptr;
     napi_get_boolean(env, status, &result);
     return result;
