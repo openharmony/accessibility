7ce7c71c39c68cff3abff40df789f8b38a7f3609
taojuncun <taojuncun@huawei.com>
2026-02-11 19:29:05 +0800

    编译报错修改
    Signed-off-by: taojuncun <taojuncun@huawei.com>

--- services/aams/src/accessibility_window_manager.cpp
+++ services/aams/src/accessibility_window_manager.cpp
@@ -306,7 +306,7 @@ void AccessibilityWindowManager::UpdateAccessibilityWindowInfo(AccessibilityWind
         accWindowInfo.SetMainWindowId(windowInfo->innerWid_);
         HILOG_DEBUG("scene board window id 1 convert inner window id[%{public}d]", windowInfo->innerWid_);
     }
-    HILOG_DEBUG("bundle name is [%{public}s] , touchHotAreas size(%{public}lu)",
+    HILOG_DEBUG("bundle name is [%{public}s] , touchHotAreas size(%{public}u)",
         windowInfo->bundleName_.c_str(), windowInfo->touchHotAreas_.size());
     accWindowInfo.SetBundleName(windowInfo->bundleName_);
     HILOG_DEBUG("UpdateAccessibilityWindowInfo is set bundlename is [%{public}s]",
@@ -1263,7 +1263,6 @@ void AccessibilityWindowManager::GetA11yWindowsBundleName(int32_t windowId, std:
         HILOG_DEBUG("GetA11yWindowsBundleName windowId:[%{public}d], BundleName:[%{public}s]",
             windowId, bundleName.c_str());
     }
-    return bundleName;
 }
 
 void AccessibilityWindowManager::SetEventInfoBundleName(AccessibilityEventInfo &uiEvent)
