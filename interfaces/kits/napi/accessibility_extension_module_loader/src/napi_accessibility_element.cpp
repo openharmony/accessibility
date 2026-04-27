b9ca3771d79d33fe09f40a1eae0081db2ab6dcfa
wwwyyyhhhh <wangyuhao62@huawei.com>
2026-04-27 21:17:53 +0800

    update: 更新文件 napi_accessibility_element.cpp
    Signed-off-by: wwwyyyhhhh <wangyuhao62@huawei.com>

--- interfaces/kits/napi/accessibility_extension_module_loader/src/napi_accessibility_element.cpp
+++ interfaces/kits/napi/accessibility_extension_module_loader/src/napi_accessibility_element.cpp
@@ -3099,9 +3099,13 @@ void NAccessibilityElement::FindElementComplete(napi_env env, napi_status status
 
     GetElement(callbackInfo, result[PARAM1]);
     if (callbackInfo->ret_ == RET_ERR_NO_WINDOW_CONNECTION && callbackInfo->systemApi) {
-        result[PARAM0] = CreateJsError(env,
-            static_cast<int32_t>(NAccessibilityErrorCode::ACCESSIBILITY_ERROR_TARGET_WINDOW_CONNECTION_FAILED),
-            ERROR_MESSAGE_TARGET_WINDOW_CONNECTION_FAILED);
+        result[PARAM0] = CreateJsError(
+            env,
+            static_cast<int32_t>(
+                NAccessibilityErrorCode::ACCESSIBILITY_ERROR_TARGET_WINDOW_CONNECTION_FAILED
+            ),
+            ERROR_MESSAGE_TARGET_WINDOW_CONNECTION_FAILED
+        );
     } else {
         result[PARAM0] = CreateBusinessError(env, callbackInfo->ret_);
     }
